c00080bb4e274c9566b2ae9f950e45396e07f7a0
AqayePardakht <aqayepardakht@gmail.com>
2021-12-30 14:23:05 +0330

    fixed bug

--- src/Service.php
+++ src/Service.php
@@ -24,7 +24,7 @@ class Service {
         $this->action      = $action;
         $this->url         = $config['url'].$config['actions'][$action]['url'].'/';
         $this->http_method = $config['actions'][$action]['method'];
-        $this->auth        = !empty($config['auth']) ? $config['auth']: null;
+        $this->auth        = !empty($config['auth']) ? $config['auth'] : null;
         // $this->timeout     = $config['timeout'];
     }
 
